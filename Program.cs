903d864f01696f6e5844b9bbba466c1f2da791e8
UfukBlbn <71414017+UfukBlbn@users.noreply.github.com>
2022-05-20 09:48:26 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+namespace GenericListCollection
+{
+
+    class Product
+    {
+        public string Name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>();
+
+            myList.Add(10);
+            myList.Add(20);
+            myList.Add(30);
+
+            List<string> myList2 = new List<string>();
+
+            myList2.Add("Ufuk");
+            myList2.Add("Yasin");
+            myList2.Add(null);
+
+            List<Product> phones = new List<Product>()
+            {
+                new Product() {Name="Samsung Galaxy s6"},
+                new Product() {Name="Samsung Galaxy Edge"},
+                new Product() {Name="Iphone X"},
+                new Product() {Name="Xiaomi"}
+
+            };
+
+             List<Product> phones2 = new List<Product>()
+            {
+                new Product() {Name="Huawei s6"},
+                new Product() {Name="Ipad"},
+                new Product() {Name="Iphone X"},
+                new Product() {Name="Xiaomi"}
+
+            };
+
+            phones.AddRange(phones2);
+            
+        }
+    }
+}
